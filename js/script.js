dc2886a2aae6bf8b74e5c84720b1358054269a04
James Bremner <j.bremner@ymail.com>
2024-02-19 16:29:29 +0000

    add list element

--- js/script.js
+++ js/script.js
@@ -11,13 +11,37 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-
-
+//console.log(data);
+const itemsPerPage = 8;
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const showPage = (list, page) => {
+  const startIndex = (page * itemsPerPage) - itemsPerPage;
+  const endIndex = page * itemsPerPage;
+  const studentList = document.querySelector('.student-list');
+  studentList.innerHTML = "";
+  for(let i = 0; i < list.length; i++) {
+    const student = list[i];
+    if(i >= startIndex && i < endIndex) {
+      const studentItem = document.createElement('li');
+      studentItem.classList.add('student-item');
+      studentItem.classList.add('cf');
+      studentItem.innerHTML = `<div class="student-details">
+      <img class="avatar" src="${ student.picture.medium }" alt="${ student.name.first }">
+      <h3>${ student.name.first } ${ student.name.last }</h3>
+      <span class="email">${ student.email }</span>
+    </div>
+    <div class="joined-details">
+      <span class="date">Joined ${ student.registered.date }</span>
+    </div>`
+    };
+    studentList.insertAdjacentElement('afterbegin', studentItem);
+  }
+}
 
+showPage(data, 1);
 
 
 /*
